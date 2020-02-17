43dddd1a19d18e396da48ac38f5983e2765194fa
Timothy <timothy@unnotech.com>
2020-02-17 18:26:40 +0800

    fix(config): fix typo

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ export default {
   baseUrl: '/news',
   api: {
     origin: 'https://newsapi.org/v2/',
-    toekn: 'a1e0f3427bfd466988c4a19c0c7095f5'
+    token: 'a1e0f3427bfd466988c4a19c0c7095f5'
   }
 }
